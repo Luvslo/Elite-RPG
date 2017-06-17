9d2ecb5fd61df6a99a3d0937b3fb60b40ed43757
theHeadTy <theHeadTy@gmail.com>
2017-06-16 22:37:43 -0400

    updated

--- database/factories/MapFactory.php
+++ database/factories/MapFactory.php
@@ -8,3 +8,16 @@ $factory->define(App\Map::class, function (Faker\Generator $faker) {
         'description' => $faker->sentence
     ];
 });
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory - App\Mov */
+$factory->define(App\Mob::class, function (Faker\Generator $faker) {
+
+    return [
+        'map_id' => $faker->randomDigit,
+        'name' => $faker->name,
+        'image' => $faker->image,
+        'type' => 'mob',
+        'x' => 150,
+        'y' => 150,
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,8 +11,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        //$this->call(UsersTableSeeder::class);
 
-        $this->call(MapsTableSeeder::class);
+        //$this->call(MapsTableSeeder::class);
+
+        $this->call(MobsTableSeeder::class);
     }
 }
